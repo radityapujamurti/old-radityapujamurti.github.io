e45903fa54c6f8152066d1a8774356042be01024
radityapujamurti <radityapujamurti@gmail.com>
2016-05-27 13:09:34 +0800

    fixed prev

--- script.js
+++ script.js
@@ -21,9 +21,8 @@ $(document).ready(function(){
       window.location.hash = hash;
     });
   });
-})
 
-//google-analytics
+  //google-analytics
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -31,4 +30,7 @@ $(document).ready(function(){
 
   ga('create', 'UA-78424011-1', 'auto');
   ga('send', 'pageview');
+})
+
+
 
