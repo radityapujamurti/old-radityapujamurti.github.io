e9358feb0a11ded245bd218066ee1c2e1593a0d4
radityapujamurti <radityapujamurti@gmail.com>
2016-06-08 13:11:00 +0800

    added slide anim effect in life item

--- life/script.js
+++ life/script.js
@@ -10,3 +10,14 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 
 }
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  })
