5330ff836019045ef8c426fd3ef47a93de1479eb
radityapujamurti <radityapujamurti@gmail.com>
2017-06-05 12:13:22 +0800

    edited home section

--- script.js
+++ script.js
@@ -32,5 +32,14 @@ $(document).ready(function(){
   ga('send', 'pageview');
 })
 
+$(document).ready(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
 
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  })
 
